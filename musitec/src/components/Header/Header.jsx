1da673a2771e57cc602ba84f81bebdf63dbac4c8
azufreylavaII <cibertec986067353@gmail.com>
2024-08-16 22:13:30 +0000

    primer paso base

--- musitec/src/components/Header/Header.jsx
+++ musitec/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-//import './Header.css';
+import './Header.css';
 import { Link } from 'react-router-dom';
 
 function Header() {
@@ -9,9 +9,11 @@ function Header() {
       <nav>
         <ul>
           <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/about">Sobre Nosotros</Link></li>
-          <li><Link to="/store">Tienda</Link></li>
-          <li><Link to="/services">Servicios</Link></li>
+          <li><Link to="/about">Nosotros</Link></li>
+          <li><Link to="/careers">Carreras</Link></li>
+          <li><Link to="/courses">Cursos</Link></li>
+          <li><Link to="/agreements">Convenios</Link></li>
+          <li><Link to="/news">Noticias</Link></li>
         </ul>
       </nav>
     </header>
@@ -19,3 +21,6 @@ function Header() {
 }
 
 export default Header;
+
+
+

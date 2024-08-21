d1d60344746949e92cca370fa23f3467000585f2
azufreylavaII <cibertec986067353@gmail.com>
2024-08-21 19:21:39 +0000

    funcional 21082024 1421

--- musitec/src/pages/CareerPage/CareerPage.jsx
+++ musitec/src/pages/CareerPage/CareerPage.jsx
@@ -1,16 +1,14 @@
 import { Fragment } from 'react';
 import React from 'react';
 import './CareerPage.css'
-import TitleCareer from '../../components/Careers/TitleCareer';
-import CareerCards from '../../components/Careers/Careercards';
+
 
 function CareerPage (){
 
     return(
     <>
         <div>
-            <TitleCareer />
-            <CareerCards />
+          
         </div>
     </>
     );

97202622b2812377fcc7834a1effeb27ad1e0355
azufreylavaII <cibertec986067353@gmail.com>
2024-08-21 21:22:25 +0000

    InicioData.css , InicioData.jsx  contenido agregado .HomePage.css y jsx  contenido modificado

---
+++ musitec/src/components/inicio/InicioData/InicioData.jsx
@@ -0,0 +1,25 @@
+
+import './InicioData.css'
+
+function InicioData(){
+    return(<>
+     <section class="section__container data__container">
+    <div class="data__content">
+      <div class="data__card">
+        <h4>150+</h4>
+        <p>Aulas especiales</p>
+      </div>
+      <div class="data__card">
+        <h4>15+</h4>
+        <p>Modernas salas de ensayo</p>
+      </div>
+      <div class="data__card">
+        <h4>500+</h4>
+        <p>Egresados exitosos</p>
+      </div>
+    </div>
+  </section>
+
+    </>);
+}
+export default InicioData;
--- musitec/src/pages/HomePage/HomePage.jsx
+++ musitec/src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import InicioUs from '../../components/inicio/InicioUs/InicioUs';
 import InicioServicesSection from '../../components/inicio/InicioServicesSection/InicioServicesSection';
 import InicioBenefits from '../../components/inicio/InicioBenefits/InicioBenefits';
+import InicioData from '../../components/inicio/InicioData/InicioData';
 
 function HomePage() {
   return (
@@ -9,6 +10,7 @@ function HomePage() {
       <InicioUs/>
       <InicioServicesSection/>
       <InicioBenefits/>
+      <InicioData/>
 
     </div>
   );

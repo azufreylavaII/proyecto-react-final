e10f34b7f8853e60b29d59cb14f44060f5daaa61
azufreylava <2luis986067353@gmail.com>
2024-08-27 13:41:53 -0500

    sec

--- musitec/src/pages/AboutPage/AboutPage.jsx
+++ musitec/src/pages/AboutPage/AboutPage.jsx
@@ -3,6 +3,7 @@ import AboutContenido from '../../components/About/AboutContenido/AboutContenido
 import './AboutPage.css'
 import InicioSlider from '../../components/inicio/InicioSlider/InicioSlider';
 //lasanbbb
+//secnd
 function AboutPage() {
   return (
 <>

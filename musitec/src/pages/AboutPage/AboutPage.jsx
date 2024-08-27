048b2961dd02ade0dc5b615fc453838daf047fe6
azufreylava <2luis986067353@gmail.com>
2024-08-27 12:52:16 -0500

    about update

--- musitec/src/pages/AboutPage/AboutPage.jsx
+++ musitec/src/pages/AboutPage/AboutPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AboutContenido from '../../components/About/AboutContenido/AboutContenido';
 import './AboutPage.css'
 import InicioSlider from '../../components/inicio/InicioSlider/InicioSlider';
-//se  agrego el slider en about.
+//lasanbbb
 function AboutPage() {
   return (
 <>
